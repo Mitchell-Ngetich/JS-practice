cb5cf323125404aa0b003c2ac56f8532efe48f6e
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-07-09 21:59:56 +0300

    code challenge 2

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
  'use strict';
 
+ // Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an 
+event listener that changes the color of the selected h1 element 
+('header') to blue, each time the BODY element is clicked. Do NOT 
+select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! 
+Take all the time you need. Think about WHEN exactly the callback function
+ is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+   document.querySelector("body").addEventListener("click", function(){
+    header.style.color = "blue"
+   })
+});
+
  // Coding Challenge #1
 
 /* 
 Let's build a simple poll app!
 
-A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+A poll has a question, an array of options from
+ which people can choose, and an array with the 
+ number of replies for each option. This data is 
+ stored in the starter object below.
 
 Here are your tasks:
 
-1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
-  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+1. Create a method called 'registerNewAnswer' 
+on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to
+   input the number of the selected option. The 
+   prompt should look like this:
         What is your favourite programming language?
         0: JavaScript
         1: Python
@@ -18,10 +49,20 @@ Here are your tasks:
         3: C++
         (Write option number)
   
-  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+  1.2. Based on the input number, update the answers array. 
+  For example, if the option is 3, increase the value AT 
+  POSITION 3 of the array by 1. Make sure to check if the 
+  input is a number and if the number makes sense 
+  (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
-4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+3. Create a method 'displayResults' which displays the poll results.
+ The method takes a string as an input (called 'type'), which can be
+  either 'string' or 'array'. If type is 'array', simply display the 
+  results array as it is, using console.log(). This should be the default
+   option. If type is 'string', display a string like "Poll results are 
+   13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' 
+method call.
 
 HINT: Use many of the tools you learned about in this and the last section 😉
 
@@ -32,6 +73,79 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: [
+         "JavaScript",
+         "Python",
+         "Rust",
+         "C++"
+    ],
+    answers: [],
+    registerNewAnswer(){
+        const myOptions = this.options;
+        const promptMessage = `${this.question}\n${myOptions.map((option, index) => `${index}: ${option}`).join('\n')}\n(Write option number)`;
+    // const answer = prompt(promptMessage);
+    }
+}
+poll.registerNewAnswer()
+
+// CLOSURES
+function secureBooking(){
+  let passengerCount = 0;
+  return function (){
+    passengerCount++;
+    console.log(`${passengerCount} total members`)
+  }
+}
+ const book = secureBooking()
+//  the book now becomes a function
+// if you console.log, it will dispaly the content of the function
+// so instead you store them in a variable
+ book()
+ book()
+ book()
+ book()
+ book()
+
+ let f;
+
+ function g(){
+  const x = 12;
+   f = function (){
+    console.log(x * 3)
+   }
+ }
+g();
+f() //> 36
+
+function y(){
+  const a = 356;
+  f = function (){
+    console.log(a * 2)/*if i put x instead of a, 
+    it will throw an error because it doesn't access x
+*/
+   }
+}
+y()
+f() //> 712
+
+function bordingPssenger(n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function(){
+    console.log(`we will be boarding all the ${n} passengers`);
+    console.log(`There are 3 groups each with ${perGroup} members`)
+  }, wait * 0);
+  console.log(`Will start boarding in ${wait} seconds`)
+};
+
+bordingPssenger(360, 2)
+/*will start...message is executed first because of asynch JS, even if
+timeout is 0,It's important to note that even with a 0 delay, 
+the setTimeout callback function is still executed asynchronously, 
+meaning it will be scheduled to run separately from the normal execution
+ flow of the code */
 
 // const booking = [];
 // // Default parameters when you assign the values
