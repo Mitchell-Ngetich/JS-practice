7404825e4b353d8384081572354a27f0b8973de7
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-08-15 16:22:49 +0300

    OOP is tough

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 'use strict';
 
 ///////////////////////////////////////
+
+
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current 
+speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed
+ in mi/h (but converts it to km/h before storing the value, 
+ by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and 
+brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+// class Challenge {
+//     constructor(make, speed){
+//        this.make = make;
+//         this.speed = speed;
+//     }
+//     accelerate(){
+//         this.speed += 10; 
+//         console.log(`${this.make} has a speed of ${this.speed}`)
+//     }
+
+//     brake(){
+//         this.speed -= 5;
+//         console.log(`${this.make} has a speed of ${this.speed}`)
+//     }
+
+//     get speedUS(){
+//         return this.speed / 1.6;
+//     }
+//     set speedUS(speed){
+//         this.speed = speed * 1.6
+//     }
+// }
+
+// const ford = new Challenge("Ford", 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 60;
+// console.log(ford)
 // Coding Challenge #1
 
 /* 
@@ -25,7 +75,7 @@ GOOD LUCK 😀
 //     this.speed = speed
 // }
 // Car.prototype.accelerate = function(){
-//     //* we use this keyword to refer to the object that was calle on 
+//     //* we use this keyword to refer to the object that was called on 
 //     //* in this case bmw.speed or merc.speed
 //    this.speed += 10;
 //    console.log(`${this.make} ${this.speed}`)
@@ -83,49 +133,51 @@ GOOD LUCK 😀
 
 //Classes in EX6
 
-class Person{
-    constructor(fullName, birthYear){
-        this.fullName = fullName;
-        this.birthYear = birthYear
-    }
+// class Person{
+//     constructor(fullName, birthYear){
+//         this.fullName = fullName;
+//         this.birthYear = birthYear
+//     }
 
-    //inherited by the instances
-    calcAge(){
-        console.log(2023 - this.birthYear)
-    }
-    hobby(){
-        console.log(`${this.fullName} loves watching`)
-    }
-    get age(){
-        return 2023 - this.birthYear
-    }
+//     //inherited by the instances
+//     calcAge(){
+//         console.log(2023 - this.birthYear)
+//     }
+//     hobby(){
+//         console.log(`${this.fullName} loves watching`)
+//     }
+//     get age(){
+//         return 2023 - this.birthYear
+//     }
 
-    //set a property that already exists.
-    set fullName(name){
-        console.log(name)
-        if (name.includes(" ")) this._fullname = name;
-        else alert(`${name} is not a full name`)
-    }
-    get  fullName(){
-        return this._fullName
-    }
+//     //set a property that already exists.
+//     set fullName(name){
+//         console.log(name)
+//         if (name.includes(" ")) this._fullname = name;
+//         else alert(`${name} is not a full name`)
+//     }
+//     get  fullName(){
+//         return this._fullName
+//     }
 
-    //static methods(cannot be inherited by the instances)
-    static hey(){
-        console.log("Hey there ✋")
-    }
-}
+//     //static methods(cannot be inherited by the instances)
+//     static hey(){
+//         console.log("Hey there ✋")
+//     }
+// }
 
-Person.hey();
-const pepe = new Person("Nehema Jerube", 2014);
-// pepe.hey()//will throw an error coz its a static method
+// Person.hey();
+// const pepe = new Person("Nehema Jerube", 2014);
+// // pepe.hey()//will throw an error coz its a static method
 
 
-const mitch = new Person("Mitchell Ngetich", 1997);
-console.log(mitch._name)
+// const mitch = new Person("Mitchell Ngetich", 1997);
+// console.log(mitch._name)
 
-const joy = new Person("Joy Chumba", 2015)
-console.log(joy)
+// const joy = new Person("Joy Chumba", 2015)
+// console.log(joy)
+
+
 // const mitch = new Person("Mitchell", 1997);
 // const joy = new Person("Joy", 2015)
 // console.log(joy)
@@ -144,14 +196,69 @@ console.log(joy)
  //3.Classes are first class citizens
 
  //Setters and Getters
- const account = {
-    owner: "Mitch",
-    movements: [200, 530, 120, 300],
+//  const account = {
+//     owner: "Mitch",
+//     movements: [200, 530, 120, 300],
 
-    get latest(){
-        return this.movements.slice(-1).pop()
-    }
- }
+//     get latest(){
+//         return this.movements.slice(-1).pop()
+//     }
+//  }
 
- console.log(account.latest)
- console.log(account.movements)
+//  console.log(account.latest)
+//  console.log(account.movements)
+
+// const PersonProto = {
+    
+//     calcAge(){
+//         console.log(2023 - this.birthYear)
+//     } 
+//     init(firstName, birthYear){
+//         this.firstName = firstName;
+//         this.birthYear = birthYear
+//     }
+// };
+// const fancy = Object.create(PersonProto);
+// fancy.name = "Fancy";
+// fancy.birthYear = 2001;
+// fancy.calcAge();
+
+// const patty = Object.create(PersonProto);
+
+// const PersonClass = {
+//     calcAge(){
+//         console.log(2023 - this.birthYear)
+//     },
+//     //instead of using mitch.name here which is not dry
+//     //we can use function can can do better
+//     myFunction(firstName, birthYear){
+//         this.firstName = firstName;
+//         this.birthYear = birthYear
+//     }
+// }
+// const mitch = Object.create(PersonClass);
+// mitch.name = "Mitch";
+// mitch.birthYear = 1997;
+// mitch.calcAge()
+// console.log(mitch)
+// const sara = Object.create(PersonClass);
+// sara.myFunction("Sara", 1990)
+// console.log(sara)
+// sara.calcAge()
+
+const Person = function(firstName, birthYear){
+    this.firstName = firstName;
+    this.birthYear = birthYear
+};
+
+Person.prototype.calcAge = function(){
+    console.log(2023 - this.birthYear)
+}
+
+const student = function(firstName, birthYear, course){
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    this.course = course;
+}
+const mike = new Student("Mike", 2020, "Computer Science")
+console.log(mike)
