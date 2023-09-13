fd17bd51091a97018c4f5a2228288a6342941fc3
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-09-13 21:52:48 +0300

    changes to old fetch

--- script.js
+++ script.js
@@ -6,62 +6,64 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // Promises and the Fetch API
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send();
+// const request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.com/v3.1/name/kenya');
+// request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+// request.addEventListener('load', function () {
+//   const [data] = JSON.parse(this.responseText);
+//   console.log(data);
 
-  const html = `
-     <article class="country">
-       <img class="country__img" src="${data.flag}" />
-       <div class="country__data">
-         <h3 class="country__name">${data.name}</h3>
-         <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} million people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages}</p>
-        <p class="country__row"><span>💰</span>${data.currencies}}</p>
-       </div>
-    </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
-
-// const renderCountry = function (data, className = '') {
 //   const html = `
-//     <article class="country">
-//       <img class="country__img" src="${data[0].flag}" />
-//       <div class="country__data">
-//         <h3 class="country__name">${data.name}</h3>
-//         <h4 class="country__region">${data.region}</h4>
+//      <article class="country">
+//        <img class="country__img" src="${data.flags.svg}" />
+//        <div class="country__data">
+//          <h3 class="country__name">${data.name.common}</h3>
+//          <h4 class="country__region">${data.region}</h4>
 //         <p class="country__row"><span>👫</span>${(
 //           +data.population / 1000000
 //         ).toFixed(1)} million people</p>
-//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//         <p class="country__row"><span>💰</span>${data.currencies[0].name}}</p>
+//         <p class="country__row"><span>🗣️</span>${data.languages}</p>
+//         <p class="country__row"><span>💰</span>${data.currencies}</p>
 //        </div>
 //     </article>`;
 //   countriesContainer.insertAdjacentHTML('beforeend', html);
 //   countriesContainer.style.opacity = 1;
-// };
+// });
 
-// function getResponse(country) {
-//   return (
-//     fetch(`https://restcountries.com/v3.1/name/${country}`)
-//       //returns a promise
-//       .then(function (response) {
-//         return response.json(); // returns a promise
-//       })
-//       .then(function (data) {
-//         console.log(data);
-//         return data; // converts the promise to data
-//         renderCountry(data[0]);
-//       })
-//   );
-// }
 
-// getResponse('portugal');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} million people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}}</p>
+       </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+function getResponse(country) {
+  return (
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      //returns a promise
+      .then(function (response) {
+        return response.json(); // returns a promise
+      })
+      .then(function (data) {
+        console.log(data);
+        return data; // converts the promise to data
+        renderCountry(data[0]);
+      })
+  );
+}
+
+getResponse('portugal');
