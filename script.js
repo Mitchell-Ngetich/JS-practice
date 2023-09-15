ec4c84eaee625c5cb6bb8d00bb7fee34f0c9e768
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-09-15 20:59:38 +0300

    debugging

--- script.js
+++ script.js
@@ -63,17 +63,16 @@ function getResponse(country) {
         renderCountry(data[0]);
         
         // country 2
+        if(!data[0].borders) throw new Error("No neighbour found!");
         const neighbour = data[0].borders[0];
-        console.log(neighbour);
-        if(!neighbour) throw new Error("No neighbour found!");
-
-        // if(!neighbour) throw new Error("No neighbour found!");
+        /*if you move neighbour variable b4 if statement, it will be undefined
+        thus it won't log the required error message*/
 
          return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, "Country not found");
       })
       .then(data => renderCountry(data[0], "neighbour"))
-      .catch(err => {console.error(`${err}`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again`)})
+      .catch(err => {;
+      renderError(`Something went wrong, ${err.message}. Try again`)})
       .finally(() => {
         countriesContainer.style.opacity = 1;
       })
@@ -89,13 +88,11 @@ function getResponse(country) {
      In the renderCountry function, you are passing data directly, and 
      it's assumed that it's already an object containing country information,
       not an array. Therefore, you see the object structure directly.
-      */
-      
-     
+      */  
  }
 
  btn.addEventListener("click", function () {
-   getResponse("australia");
+   getResponse("madagascar");
  });
 
 ///////////////////////////////////////
